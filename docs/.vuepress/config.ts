989d3d14edad8c9eedf7431bbc2259439566364c
leslie <leslie_ding@163.com>
2022-06-11 16:34:26 +0800

    [Update]为什么favicon.ico更新了但是网页source里找不到?_2

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -9,7 +9,7 @@ export default defineUserConfig({
 
   base: "/blog/",
 
-  head: [['link', { rel: 'icon', href: '/images/tab_logo.ico' }]],
+  head: [['link', { rel: 'icon', href: '/images/favicon.ico' }]],
 
   theme,
   
